b54e641e20f198d531fce01feb84c2d1f11bf972
Adam Combs <adam.combs.osv@fedex.com>
2025-12-08 19:37:29 -0500

    angular 21

--- src/app/tool-bar/tool-bar.ts
+++ src/app/tool-bar/tool-bar.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
-import { MatToolbar } from '@angular/material/toolbar';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-tool-bar',
   imports: [
     MatIconModule,
-    MatToolbar
+    MatToolbarModule
   ],
   templateUrl: './tool-bar.html',
   styleUrl: './tool-bar.scss',
